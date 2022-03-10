ae5ae1c2c03a3bdbe47c432e19378051b97cb254
dhineshsridharan <dhineshsridharan@gmail.com>
2022-03-10 15:40:11 +0530

    Java

---
+++ src/com/selenium/Sca_nner.java
@@ -0,0 +1,15 @@
+package com.selenium;
+
+import java.util.Scanner;
+
+public class Sca_nner {
+	
+	public static void main(String[] args) {
+		
+		Scanner c = new Scanner(System.in);
+		
+		System.out.println("Enter your name:");
+		String b = c.nextLine();
+	}
+
+}
---
+++ src/com/selenium/ScannerDemo.java
@@ -0,0 +1,16 @@
+package com.selenium;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+	
+	public static void main(String[] args) {
+		Scanner sc = new scanner(System.in);
+		
+		System.out.println("Enter your add");
+		string c = sc.nextLine();
+		System.out.println("the add is : "+c);
+		
+	}
+
+}
